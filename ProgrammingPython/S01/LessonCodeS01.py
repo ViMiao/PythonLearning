fee73b2bff2172447ca82dc666ab483538ef0c88
何力 <vigilant@live.cn>
2017-01-23 00:13:57 +0800

    DataBase Begin

--- ProgrammingPython/S01/LessonCodeS01.py
+++ ProgrammingPython/S01/LessonCodeS01.py
@@ -4,7 +4,7 @@ NAME, AGE, PAY, DEPARTMENT = range(4)
 # bob = ['Bob Smith', 42, 30000, 'Software']
 # sue = ['Sue Jones', 45, 40000, 'Hardware']
 def splitLine(total = 1):
-    for i in range(1):
+    for i in range(total):
         print('==========')
 
 bob = [['name', 'Bob Smith'], ['age', 42], ['pay', 10000]]
@@ -26,7 +26,7 @@ for person in people:
     person[2][1] *= 1.10
     print(person[2])
 
-splitLine()
+splitLine(5)
 
 for person in people:
     for (name, value) in person:
